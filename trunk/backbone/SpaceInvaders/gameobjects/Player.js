bba0fa6d0152c97f328f1576b32d422ee29833dc
achen5671 <achen5671@3c877bcc-4e1e-4f1f-aa79-bef12059953a>
2022-03-30 00:09:55 +0000

    Update Player.js, set temp player img, player position

--- trunk/backbone/SpaceInvaders/gameobjects/Player.js
+++ trunk/backbone/SpaceInvaders/gameobjects/Player.js
@@ -1,35 +1,35 @@
-"using strict";
+class Player {
+	constructor() {
 
-var gameObjs = (function (gameObjs)
-{
-    function Player_prototype ()
-    {
-        this.sprite = null;
-        this.rectangle = null;
-    }
-    Player_prototype.prototype.initialize = function(sprite, rect)
-    {
-        this.sprite = sprite;
-        this.rectangle = rect;
-    };
-    Player_prototype.prototype.drawPlayer = function()
-    {
-        brain.Canvas2d.drawRectangle(this.rectangle, "green");
-    };
-    Player_prototype.prototype.move = function (units, direction)
-    {
-        if (direction === "left")
-        {
-            var x = this.rectangle.x-units < 0 ? 0 : this.rectangle.x-units;
-            this.rectangle = new brain.Rectangle(x, this.rectangle.y, this.rectangle.width, this.rectangle.height);
-        }
-        if (direction === "right")
-        {
-            var future = this.rectangle.x+units+this.rectangle.width;
-            var x = future > brain.Game.dimension.x ? brain.Game.dimension.x-this.rectangle.width : this.rectangle.x+units;
-            this.rectangle = new brain.Rectangle(x, this.rectangle.y, this.rectangle.width, this.rectangle.height);
-        }
-    };
-    gameObjs.Player = new Player_prototype();
-    return gameObjs;
-})(gameObjs || {});
+		const image = new Image();
+		image.src = '../Images/player.png //placeholder, file path and image will change
+		image.onload = () => {
+			this.image = image;
+			this.width = image.width;
+			this.height = image.height;
+
+			// TODO: Update position using canvas dimensions
+			// Set player start point to bottom mid
+			this.position = {
+				x: 0, 
+				y: 0,
+			}
+		}
+		
+		// TODO: Update position using canvas. 
+		// Set player start point to bottom mid 
+		this.position = {
+			x: 0,
+			y: 0,
+		}
+
+		this.velocity = {
+			x: 0,
+			y: 0,
+		}
+
+		this.playerImage = playerImage;
+		this.width = 100;
+		this.height = 100;
+	}
+}
