0f81992a0497d3bc8802dddc53770b43498d2cb3
achen5671 <achen5671@3c877bcc-4e1e-4f1f-aa79-bef12059953a>
2022-03-31 16:10:18 +0000

    move move() and shoot() temp

--- backbone/SpaceInvaders/gameobjects/Player.js
+++ backbone/SpaceInvaders/gameobjects/Player.js
@@ -6,7 +6,6 @@ canvas.height = innerHeight
 
 class Player {
 	constructor() {
-
 		const image = new Image();
 		image.src = '../Images/player.png //placeholder, file path and image will change
 		image.onload = () => {
@@ -22,40 +21,6 @@ class Player {
 				y: canvas.height - this.height - 50,
 			}
 		}
-
-		//set player lives to 3
-		this.lives = 3;
-		//create variable to change player position on X axis
-		this.velocityX = 0;
-		//variables for shooting
-		this.projectile = [];
-		this.shotdelay = 0;
-		//TODO: Finish function to move player based on keyboard input
-		function move(input){
-			//key input for left is 37
-			if(input == 37){
-				//moves left by subtracting from it's x position
-				velocityX = -1;	
-			}
-			//key input for right is 39
-			if(input == 39){
-				//moves right by adding to it's x position
-				velocityX = 1;
-			}
-			this.x += velocityX;
-			velocityX = 0;
-		}
-		//TODO: Create function to spawn projectile and shoot it
-		function shoot(input){
-			//key input for space is 32
-			if(input == 32){
-				//TODO: spawn a projectile
-				
-				//set shot delay to 0.5 secs
-				this.shotdelay = 0.5;
-			}
-		}
-		//TODO: create hitbox to detect when player gets shot
 	}
 
 	draw() {
