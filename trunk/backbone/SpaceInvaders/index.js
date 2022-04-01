0017f3ce9498e55c0e1c364bf66494249fde3a96
achen5671 <achen5671@3c877bcc-4e1e-4f1f-aa79-bef12059953a>
2022-04-01 16:10:58 +0000

    Add missiles for player, shooting logic, added readme

--- trunk/backbone/SpaceInvaders/index.js
+++ trunk/backbone/SpaceInvaders/index.js
@@ -1,24 +1,39 @@
 import Player from "./gameobjects/Player.js"
-import { canvas, c } from "../models/Canvas.js"
+import Missile from "./gameobjects/Missile.js"
+
+// canvas is used in multiples files. Follow DRY
+// TODO: create nodes or package files to import canvas
+const canvas = document.getElementById('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = innerWidth
+canvas.height = innerHeight
 
 const player = new Player()
+const missiles = []
 
 // Maybe do two player mode if we have time?
+// TODO: Move to utils folder
 const keys = {
-    a: {pressed: false},
-    d: {pressed: false},
     ArrowLeft: {pressed: false},
     ArrowRight: {pressed: false},
+    a: {pressed: false},
+    d: {pressed: false},
     space: {pressed: false}
 }
 
-function move() {
-	requestAnimationFrame(move)
+// TODO: Move to Player.js
+// animate() takes care of all animation that goes on in SpaceInvaders game.
+function animate() {
+	requestAnimationFrame(animate)
+    c.fillStyle = 'black'
 	c.fillRect(0,0, canvas.width, canvas.height) // Temporary background
 	player.update()
     
     var moveLeft = keys.a.pressed || keys.ArrowLeft.pressed
     var moveRight = keys.d.pressed || keys.ArrowRight.pressed
+
+    shoot()
 
     if (moveLeft && player.position.x >= 0) {
         player.travel.x = -5
@@ -29,8 +44,16 @@ function move() {
     }
 }
 
-move()
+// TODO: Move to Player.js
+function shoot() {
+    missiles.forEach((missile) => { // see line 59
+        missile.update()
+    })
+}
 
+animate()
+
+// Registers user key stroke. Validkeys as a, d, left arrow, right arrow
 addEventListener('keydown', ({key}) => {
     switch (key) {
         case 'a': 
@@ -45,7 +68,11 @@ addEventListener('keydown', ({key}) => {
         case 'ArrowRight': 
             keys.ArrowRight.pressed = true
             break
-        case ' ': // shoot TODO: Implement
+        case ' ': 
+            // TODO: This pushes new missles obj to missiles array. This will clog up array and slow down performance. Fix
+            missiles.push(new Missile({
+                position: {x: player.position.x + player.width / 2, y: player.position.y}, speed: {x: 0, y:-10}
+            }))
             break
     }
 })
