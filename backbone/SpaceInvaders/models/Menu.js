d0435c2dcfcd0d4711622355de9e70e440426506
achen5671 <achen5671@3c877bcc-4e1e-4f1f-aa79-bef12059953a>
2022-04-09 12:36:59 +0000

    renamed and moved styles.css. Added a Start menu

---
+++ backbone/SpaceInvaders/models/Menu.js
@@ -0,0 +1,16 @@
+
+// Does this belong in models?
+document.body.addEventListener('keypress', (key) => {
+    if(key.key == 'Enter') start()
+})
+
+function start() {
+    this.showScreen('start-screen', false)
+    this.showScreen('canvas', true)
+}
+
+function showScreen(id, isShow) {
+    let screen = document.getElementById(id)
+    let display = ( isShow ) ? 'block' : 'none'
+    screen.style.display = display 
+}
