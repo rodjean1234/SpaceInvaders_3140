beadf676c45802b2bfb24e94900d38b4884f63ed
achen5671 <achen5671@3c877bcc-4e1e-4f1f-aa79-bef12059953a>
2022-05-03 13:19:48 +0000

    Start game when button is clicked, Add TODO and comments, misc

--- trunk/backbone/SpaceInvaders/models/Canvas.js
+++ trunk/backbone/SpaceInvaders/models/Canvas.js
@@ -2,5 +2,6 @@ export const canvas = document.getElementById('canvas')
 export const cv = canvas.getContext('2d')
 
 // #TODO: Do we need innerHeight and innerWidth equal canvas height and width?
+// Fixed canvas inorder to support most browser size
 canvas.width = 1360
 canvas.height = 768
